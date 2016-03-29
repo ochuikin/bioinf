18f792cb1de390437642df2758af0c8f0e8da539
ochuykin <lastvar@gmail.com>
2016-03-29 03:50:54 +0300

    task 31 doesn't work, https://github.com/minw2828/Coursera---Bioinformatics-Algorithms/blob/master/chapter5/C5_57/57_10/k-universal_circular_string.py

---
+++ src/com/babichev/oleg/bioinformatic/problems/Problem31.java
@@ -0,0 +1,12 @@
+package com.babichev.oleg.bioinformatic.problems;
+
+import com.babichev.oleg.bioinformatic.utils.ConvertUtils;
+
+/**
+ * Created by olegchuikin on 29/03/16.
+ */
+public class Problem31 {
+    public static void main(String[] args) {
+        System.out.println(ConvertUtils.universalCircularString(8));
+    }
+}
--- src/com/babichev/oleg/bioinformatic/utils/ConvertUtils.java
+++ src/com/babichev/oleg/bioinformatic/utils/ConvertUtils.java
@@ -1,5 +1,11 @@
 package com.babichev.oleg.bioinformatic.utils;
 
+import com.babichev.oleg.bioinformatic.core.BaseGenome;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by olegchuikin on 18/02/16.
  */
@@ -19,6 +25,48 @@ public class ConvertUtils {
         }
 
         return result;
+    }
+
+    public static String binaryToString(long number){
+        if (number == 0) return "";
+        return binaryToString(number / 2) + (number % 2 == 1?"1":"0");
+    }
+
+    public static String multipleString(String s, int n){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) sb.append(s);
+        return sb.toString();
+    }
+
+    public static String universalCircularString(int k){
+        List<String> ref = new ArrayList<>();
+        for (long i = 0; i < Math.pow(2, k); i++){
+            String s = binaryToString(i);
+            s = multipleString("0", k - s.length()) + s;
+            ref.add(s);
+        }
+
+        Collections.sort(ref);
+
+        List<String> ustring = new ArrayList<>();
+        int ustringLen = (int)Math.pow(2, k) + k - 1;
+        for (long i = 0; i < Math.pow(2, ustringLen); i++){
+            String s = binaryToString(i);
+            s = multipleString("0", ustringLen - s.length()) + s;
+            if (compareLists(new BaseGenome(s).getKmerComposition(k), ref)){
+                return s;
+            }
+        }
+
+        return null;
+    }
+
+    private static boolean compareLists(List<String> a, List<String> b){
+        if (a.size() != b.size()) return false;
+        for (int i = 0; i < a.size(); i++){
+            if (!a.get(i).equals(b.get(i))) return false;
+        }
+        return true;
     }
 
 }
